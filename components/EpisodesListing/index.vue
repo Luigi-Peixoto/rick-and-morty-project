d44ca94204bd806b1c49ce5316e2f9f23b92cd54
Luigi Peixoto <luigisp08@gmail.com>
2025-10-18 22:29:21 -0300

    Initial commit

---
+++ components/EpisodesListing/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="flex flex-col w-full max-w-[1224px] mx-auto gap-8">
+    <div class="flex flex-wrap gap-4 justify-center xl:justify-start">
+      <ListingHeader title="Episódios" />
+
+      <div
+        class="flex gap-4 flex-wrap justify-center lg:grid-cols-[repeat(5,1fr)]"
+      >
+        <Card
+          v-for="currentEpisode of data.results"
+          class="flex flex-col gap-2 justify-between min-h-[150px] max-w-[250px]"
+        >
+          <div class="flex gap-2 item-center">
+            <IconsMonitorPlay class="flex-[0_0_24px]" />
+            <p>{{ currentEpisode.name }} | {{ currentEpisode.episode }}</p>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <KnowMore class="my-auto" />
+            <IconsHeartFilled :width="32" :height="32" />
+          </div>
+        </Card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import Card from '@/components/Card/index.vue';
+import KnowMore from '@/components/KnowMore/index.vue';
+import ListingHeader from '@/components/ListingHeader/index.vue';
+
+const { data } = await useFetch('https://rickandmortyapi.com/api/episode');
+</script>
